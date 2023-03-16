005246b3e03fa1a78fb887be03f429a42efda425
Aman Jaiswal <general@amanjaiswal.vip>
2023-03-16 12:47:32 -0400

    Further Configured Routes with Express

--- main.js
+++ main.js
@@ -6,51 +6,14 @@ require('dotenv').config();
 
 
 const app = express();
+app.use(express.urlencoded({ extended : true}))
 app.set('view engine', 'pug');
-const notfound = pug.compileFile('./views/404notfound.pug')
 
 
 const db = new Database();
 db.connect();
-const notfound404 = (req, res) => {
-    const heading = (title) => {
-        let html;
-        html = `
-        <!doctype html>
-            <html lang="ENGLISH">
-                <head>
-                    <title>${title}</title>
-                    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.min.css">
-                </head>
-                <body>
-                <a href='/'>Home</a>
-                <br/>
-    `;
-        return html;
-    }
 
-    const footing = () => {
-        return `
-        </body>
-    </html>
-    `;
-    }
-
-    const render = (res, html) =>{
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(html);
-        res.end();
-    }
-
-
-    let html = heading("404 Not Found");
-    html += notfound()
-    html += footing()
-    render(res, html)
-}
-
-
-app.use('/', notfound404)
+app.use('/', require('./routes/404notfound.js'))
 app.listen(3000, () => {
     console.log(`Example app listening on port 3000`)
 })
